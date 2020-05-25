0ccf007d43c74d9d29f63c82702d67d121ef8157
anil kadam <anilkadam210@gmail.com>
2020-05-25 18:49:01 +0530

    Rendering list of images retrieved from api

---
+++ src/components/ImageList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return (
+      <img src={image.urls.regular} alt={image.description} key={image.id} />
+    );
+  });
+  return <div>{images}</div>;
+};
+
+export default ImageList;

025108364a610d370968cdba45eed36b07966fd7
anil kadam <anilkadam210@gmail.com>
2020-05-25 18:07:17 +0530

    created axios client sending GET request for search term

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import Axios from "axios";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    Axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization:
+          "Client-ID 5d5433b717fc3cf5085f9c672917bba74341d463cdf25610b152c34773a3dfe2",
+      },
+    });
   }
   render() {
     return (

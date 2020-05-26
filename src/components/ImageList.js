af795436faa1ba731f9a27dc6b51d6c442ab65db
anil kadam <anilkadam210@gmail.com>
2020-05-26 22:05:03 +0530

    Added Grid css for displaying image lsit

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./ImageList.css";
 
 const ImageList = (props) => {
   const imagesDiv = props.images.map(({ description, urls, id }) => {
     return <img src={urls.regular} alt={description} key={id} />;
   });
-  return <div>{imagesDiv}</div>;
+  return <div className='image-list'>{imagesDiv}</div>;
 };
 
 export default ImageList;
